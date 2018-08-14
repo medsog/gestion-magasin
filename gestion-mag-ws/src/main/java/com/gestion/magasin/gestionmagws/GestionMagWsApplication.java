f2881aa9d8314c722ac28c5d02b42a31d8d7a68b
medsog <mohamedsoghri@gmail.com>
2018-08-14 13:12:17 +0100

    ws back office

---
+++ gestion-mag-ws/src/main/java/com/gestion/magasin/gestionmagws/GestionMagWsApplication.java
@@ -0,0 +1,12 @@
+package com.gestion.magasin.gestionmagws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMagWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMagWsApplication.class, args);
+	}
+}
---
+++ gestion-mag-ws/src/test/java/com/gestion/magasin/gestionmagws/GestionMagWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gestion.magasin.gestionmagws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionMagWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
